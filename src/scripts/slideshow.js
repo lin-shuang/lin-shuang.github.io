8a2185701b8e5aa38009b820bbf6bbc45355cc29
lin-shuang <shuanglin3359@gmail.com>
2023-09-21 05:06:08 -0400

    project 2 images, and slideshow ID fix

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -1,33 +1,37 @@
-let slideIndex = 0;
-showSlides(slideIndex);
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to control slideshow
+    function showSlides(slideIndex, slideshowId, captionId, dotsId) {
+        let i;
+        const slides = document.querySelectorAll("#" + slideshowId + " .mySlides");
+        const dots = document.querySelectorAll("#" + dotsId + " .dot");
+        const captionText = document.getElementById(captionId);
 
-function showSlides(n) {
-    let i;
-    const slides = document.querySelectorAll(".mySlides");
-    const dots = document.querySelectorAll(".dot");
-    const captionText = document.getElementById("caption");
-    
-    // Hide all slides
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        // Hide all slides
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        // Deactivate all dots
+        for (i = 0; i < dots.length; i++) {
+            dots[i].classList.remove("active");
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].classList.add("active");
+        captionText.innerHTML = slides[slideIndex - 1].querySelector("img").alt;
+
+        // Change slide every 2 seconds
+        setTimeout(function () {
+            showSlides(slideIndex, slideshowId, captionId, dotsId);
+        }, 5000);
     }
-    
-    // Deactivate all dots
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = "dot";
-    }
-    
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className = "dot active";
-    captionText.innerHTML = slides[slideIndex - 1].querySelector("img").alt;
-    
-    // Change slide every 5 seconds
-    setTimeout(function() {
-        showSlides(slideIndex);
-    }, 5000);
-}
+
+    // Start both slideshows
+    showSlides(0, "slideshow1", "caption1", "dots1");
+    showSlides(0, "slideshow2", "caption2", "dots2");
+});
