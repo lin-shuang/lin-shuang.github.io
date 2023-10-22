97e18d6286991f45648fca30996a3c4d33ee42a1
lin-shuang <shuanglin3359@gmail.com>
2023-10-22 19:07:57 -0400

    Doraemon follow cursor, sit idle, farther icons

--- src/shuang-lin/scripts/doraemon.js
+++ src/shuang-lin/scripts/doraemon.js
@@ -1,7 +1,10 @@
 let doraemonElement = document.querySelector('.doraemon');
+let heroElement = document.querySelector('.hero');
 let isAwake = false;
 let isIdle = false;
 let isWalking = false;
+let inHero = false;
+let deltaX = 0;
 
 function wakeUpDoraemon() {
     isAwake = true;
@@ -16,7 +19,15 @@ function wakeUpDoraemon() {
 function setIdleState() {
     if (isAwake) {
         isIdle = true;
-        doraemonElement.style.backgroundImage = "url('./src/shuang-lin/images/doraemon/idle/doraemon-idle.gif')"; // Doraemon goes idle
+
+        // Sit (1/3) or stand (2/3)
+        let sitOrStand = Math.floor(Math.random() * 3);
+        if(sitOrStand == 0){
+            doraemonElement.style.backgroundImage = "url('./src/shuang-lin/images/doraemon/idle/doraemon-sit.gif')";
+        }
+        else{
+            doraemonElement.style.backgroundImage = "url('./src/shuang-lin/images/doraemon/idle/doraemon-idle.gif')";
+        }
     }
     else{
         //debug
@@ -28,7 +39,7 @@ function performRandomAction() {
     if (isIdle) {
         isIdle = false;
         let randomGifNumber = Math.floor(Math.random() * 8) + 1;
-        doraemonElement.style.backgroundImage = `url('./src/shuang-lin/images/doraemon/random/doraemon-random-${randomGifNumber}.gif')`; // Perform a random action
+        doraemonElement.style.backgroundImage = `url('./src/shuang-lin/images/doraemon/random/doraemon-random-${randomGifNumber}.gif')`;
         
         // Random action play for 3s
         setTimeout(function () {
@@ -46,7 +57,7 @@ function checkStatesPeriodically() {
     //debug
     console.log("Checking...");
 
-    // Every 5s, 2/3 to walk, 1/3 to act
+    // Every 5s, 2/3 to random walk, 1/3 to random act
     setInterval(function () {
         if (isAwake && isIdle) {
             let walkOrAct = Math.floor(Math.random() * 3);
@@ -55,14 +66,14 @@ function checkStatesPeriodically() {
                 performRandomAction();
             }
             else{
-                startWalking();
+                startWalking(-1); // -1 to trigger random walk
             }
         }
-    }, 5000);
+    }, 500000); //debug
 }
 
-function startWalking() {
-    if (!isWalking && isIdle) {
+function startWalking(targetPosition) {
+    if (isAwake && isIdle) {
 
         //debug
         console.log("Walking...");
@@ -71,17 +82,30 @@ function startWalking() {
         isIdle = false;
         doraemonElement.style.backgroundImage = "url('./src/shuang-lin/images/doraemon/moving/doraemon-hop.gif')";
 
-        // Range
-        const pageWidth = document.body.clientWidth;
-        const range1 = [-pageWidth/3, -20];
-        const range2 = [20, pageWidth/3];
-        const randomRange = Math.random() < 0.5 ? range1 : range2;
-        const randomX = Math.random() * (randomRange[1] - randomRange[0]) + randomRange[0];
+        // Get doraemon position
+        const initialX = parseFloat((getComputedStyle(doraemonElement).left+50) || 0);
         
-        // Speed, distance, duration
-        let walkSpeed = 50; // Default speed
-        const initialX = parseFloat(getComputedStyle(doraemonElement).left || 0);
-        const deltaX = randomX - initialX;
+        // Follow cursor
+        if(inHero && targetPosition != -1){ 
+            
+            //debug
+            console.log("Following Cursor...");
+
+            deltaX = targetPosition - initialX;
+        }
+        // Random walk
+        else{ 
+
+            //debug
+            console.log("Random Walk...");
+
+            const pageWidth = document.body.clientWidth;
+            const range1 = [-pageWidth/3, -20];
+            const range2 = [20, pageWidth/3];
+            const randomRange = Math.random() < 0.5 ? range1 : range2;
+            const randomX = Math.random() * (randomRange[1] - randomRange[0]) + randomRange[0];
+            deltaX = randomX - initialX;
+        }
 
         // Check direction
         if(deltaX > 0){
@@ -92,6 +116,7 @@ function startWalking() {
         }
 
         // Check if should run
+        let walkSpeed = 50; // Default speed
         if(deltaX > 125){
             walkSpeed = 150;
             doraemonElement.style.backgroundImage = "url('./src/shuang-lin/images/doraemon/moving/doraemon-run.gif')";
@@ -105,7 +130,7 @@ function startWalking() {
         const animationDuration = Math.max(500, (Math.abs(deltaX) / walkSpeed) * 1000);
 
         //debug        
-        console.log("pageWidth: ", pageWidth);
+        //console.log("pageWidth: ", pageWidth);
         console.log("animationDuration: ", animationDuration);
         console.log("deltaX: ", deltaX);
 
@@ -150,4 +175,32 @@ doraemonElement.addEventListener('mouseenter', function () {
 // Check clicks to play anims
 doraemonElement.addEventListener('click', function () {
     performRandomAction();
+});
+
+
+// Doraemon follows cursor
+heroElement.addEventListener('mouseenter', function (e) {
+    inHero = true;
+});
+heroElement.addEventListener('mouseleave', function (e) {
+    inHero = false;
+});
+heroElement.addEventListener('mousemove', function (e) {
+    
+    let currentPosition = parseFloat((getComputedStyle(doraemonElement).left) || 0);
+    if (inHero) {
+ 
+        // Calculate the cursorX as the distance between the cursor's x-coordinate and page center
+        const targetX = e.pageX - document.body.clientWidth/2;
+    
+        // Check if the cursor has moved over 50px
+        if (Math.abs(targetX) >= 20) {
+            startWalking(targetX);
+        }
+        //debug
+        console.log("Entered Hero...");
+        console.log("currentPosition:'", currentPosition);
+        console.log("targetX:'", targetX);
+        //console.log("distance:'", distance);
+    }
 });
